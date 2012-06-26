93800dd613092cdadd2b4023100131c192a22974
Tonin De Rosso Bolzan <admin@tonybolzan.com>
2012-06-26 00:12:54 -0300

    Arrumado detalhes de amostragem

--- public_html/protected/views/layouts/main.php
+++ public_html/protected/views/layouts/main.php
@@ -11,10 +11,6 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/functions.
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo Yii::app()->name;?>">
     <meta name="author" content="odig.net">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
     <title><?php echo Yii::app()->name . ' - ' . CHtml::encode($this->pageTitle); ?></title>
     <!--[if lt IE 9]>
       <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/html5shiv.min.js"></script>
